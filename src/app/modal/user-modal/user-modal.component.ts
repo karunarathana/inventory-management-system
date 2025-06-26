cfda76bcbb95862f7ef2e7199e316423b338703a
Nipun_112467 <nipun.sandeepa@axiatadigitallabs.com>
2025-06-26 11:41:16 +0530

    Fix: deployed v1:4

---
+++ src/app/modal/user-modal/user-modal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-modal',
+  imports: [],
+  templateUrl: './user-modal.component.html',
+  styleUrl: './user-modal.component.css'
+})
+export class UserModalComponent {
+
+}
